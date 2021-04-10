6380946a98f3f139984ce49ecba98ffc3332ffad
NicolasBorda96 <nicolas.borda@unillanos.edu.co>
2021-04-10 11:04:42 -0500

    ejercicio variables

---
+++ 1.variables/variable.dart
@@ -0,0 +1,11 @@
+void main() {
+  String nombre = "Juan";
+  int edad = 23;
+  String telefono = "3102912482";
+
+  var apellido;
+  print(nombre);
+  print(apellido);
+  print(edad);
+  print(telefono);
+}
