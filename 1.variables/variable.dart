9528f57c68cd4cd25bc2d6ba82d8d80e26c30dc4
NicolasBorda96 <nicolas.borda@unillanos.edu.co>
2021-04-10 11:28:19 -0500

    Variables

--- 1.variables/variable.dart
+++ 1.variables/variable.dart
@@ -1,11 +1,16 @@
 void main() {
-  String nombre = "Juan";
+  String nombre = "Nicolas";
   int edad = 23;
   String telefono = "3102912482";
+  int? variableAceptaNull = null;
+
+  final String segundoApellido = "Camacho";
 
   var apellido;
   print(nombre);
   print(apellido);
   print(edad);
   print(telefono);
+  print(variableAceptaNull);
+  print(segundoApellido);
 }
